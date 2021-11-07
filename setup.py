b3a0337ce756ca694936671690d54e490f67e0d7
deniskhodus <deniskhodus@gmail.com>
2021-11-07 13:43:35 +0500

    added front:FormTitle

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 
-VERSION = '0.2.8'
+VERSION = '0.2.8-1'
 
 
 with open("README.md", 'r') as f:
---
+++ wefram/frontend/components/Forms/FormTitle.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import {Box, FormItem, FormItemProps, Typography, DividerRuler} from 'system/components'
+import {Variant} from '@mui/material/styles/createTypography'
+import {FormFieldCommon} from './types'
+
+
+export type FormTitleProps = FormFieldCommon & {
+  color?: 'primary' | 'secondary' | 'default'
+  dense?: boolean
+  title: string
+  variant?: Variant
+}
+
+export type FormTitleFieldProps = FormTitleProps & FormItemProps & {
+  ref?: React.LegacyRef<FormTitle>
+  fieldStyle?: React.CSSProperties
+}
+
+
+export class FormTitle extends React.Component<FormTitleProps> {
+
+  render() {
+    return (
+      <Box
+        mt={this.props.dense ? 1 : 3}
+        mb={this.props.dense ? 0 : 2}
+      >
+        <Typography
+          color={this.props.color}
+          variant={this.props.variant}
+          gutterBottom
+        >
+          {this.props.title}
+        </Typography>
+        <DividerRuler vspace={this.props.dense ? 1 : 2} />
+      </Box>
+    )
+  }
+
+}
+
+
+export class FormTitleField extends React.Component<FormTitleFieldProps> {
+
+  render() {
+    const {
+      width,
+      pt,
+      pb,
+      pl,
+      pr,
+      p,
+      align,
+      justify,
+      fieldStyle,
+      ...elementProps
+    } = this.props
+    const formItemProps = {
+      width,
+      pt,
+      pb,
+      pl,
+      pr,
+      p,
+      align,
+      justify,
+      style: fieldStyle
+    }
+    return (
+      <FormItem {...formItemProps}>
+        <FormTitle {...elementProps} />
+      </FormItem>
+    )
+  }
+
+}
