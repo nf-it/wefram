73ae4b8b3a2c2685e7dabd976ab55a85dcc9107e
deniskhodus <deniskhodus@gmail.com>
2022-01-30 00:47:54 +0500

    detailing comments

--- wefram/frontend/dialog.tsx
+++ wefram/frontend/dialog.tsx
@@ -6,6 +6,35 @@
  * textual contents and some actions (at least - the "Close" button). The
  * most common cases are some kind of notification or warning, and the
  * confirmation dialog (Yes-No variant).
+ *
+ * Common case are, for example, displaying dialogs with text messages,
+ * or displaying confirmations.
+ *
+ * For example - the common text dialog:
+ *
+ * ``
+ *    import {dialog} from 'system/dialog'
+ *
+ *    ...
+ *    dialog.showMessage("Hello, I glad to see you", "Hello")
+ *    ...
+ * ``
+ *
+ * Or, the second example - show the confirmation:
+ *
+ * ``
+ *    import {dialog} from 'system/dialog'
+ *
+ *    ...
+ *    dialog.showConfirm({
+ *      message: "Are you sure to delete those files?",
+ *      title: "Deletion",
+ *      okCallback: () => {
+ *        dialog.hide()
+ *        ... // some staff on OK button clicked
+ *      }
+ *    })
+ * ``
  */
 
 import React from 'react'
