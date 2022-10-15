1f08f3f146b2557b5e6a228461c452a5560d4ecb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 10:23:56 +0000

    Adding tarfile member sanitization to extractall()

--- wefram/manage/platform.py
+++ wefram/manage/platform.py
@@ -95,7 +95,26 @@ async def extract_release(version: str) -> None:
     with tarfile.open(release_fn, 'r') as archive:
         members: List[tarfile.TarInfo] = archive.getmembers()
         release_pakdirname: str = (members[0].name.split('/'))[0]
-        archive.extractall(releases_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(archive, releases_path)
     os.rename(
         os.path.join(releases_path, release_pakdirname),
         os.path.join(releases_path, version)
